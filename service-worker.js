c8c534b933ef547ca877467b0802891955516976
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2020-09-10 00:14:18 +0000

    add manifest and favs

--- service-worker.js
+++ service-worker.js
@@ -22,7 +22,17 @@ const filesToCache = [
     'js/all.min.js',
     'js/main.js',
     '404.html',
-    'offline.html'
+    'offline.html',
+    'android-chrome-192x192.png',
+    'android-chrome-512x512.png',
+    'apple-touch-icon.png',
+    'browserconfig.xml',
+    'favicon.ico',
+    'favicon-16x16.png',
+    'favicon-32x32.png',
+    'mstile-150x150.png',
+    'safari-pinned-tab.svg',
+    'site.webmanifest'
   ];
   
   const staticCacheName = 'weather-cache-v1';
