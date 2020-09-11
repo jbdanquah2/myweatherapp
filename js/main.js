7eeb44957a8ccdcfdc106426504eb53cbc5003ed
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2020-09-11 20:03:14 +0000

    final

--- js/main.js
+++ js/main.js
@@ -1,44 +1,49 @@
+// register service worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('service-worker.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  }
-  
+}
 
+const api = {
+    apikey: '5290defddab27e09fb2b75fd44ba25f4',
+    cUrl: 'https://api.openweathermap.org/data/2.5/weather?',
+    dUrl: 'https://api.openweathermap.org/data/2.5/onecall?',
+    iconUrl: 'https://openweathermap.org/img/wn/'
+
+}
+
+const weatherByName = (timezone) => {
+    return `${api.cUrl}q=${timezone}&APPID=${api.apikey}&units=metric`;
+}
+
+const weatherByCoord = (lat, lon) => {
+    return [`${api.cUrl}lat=${lat}&lon=${lon}&APPID=${api.apikey}&units=metric`,
+    `${api.dUrl}lat=${lat}&lon=${lon}&appid=${api.apikey}&units=metric&cnt=8`]
+}
 
 // run getLocation when pages loads... 
 //this will also run getWeather with the location of the user...
 //obtained from getLocation
 window.onload = function () {
-    getLocation();
-    myFunction(); 
+    init();
+    // myFunction(); 
 };
 
-// this gets the location of the user when the page loads
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
+const init = () => {
+    const data = localStorage.getItem('weatherData') ? JSON.parse(localStorage.getItem('weatherData')) : {};
+    const data2 = localStorage.getItem('dailyWeather') ? JSON.parse(localStorage.getItem('dailyWeather')) : {};
+    if (data !== {}) { getCurrentData(data) };
+    if (data2 !== {}) { getDailyData(data2) };
+    console.log('local storage: ', JSON.parse(localStorage.getItem('weatherData')));
+    console.log('local storage22: ', JSON.parse(localStorage.getItem('dailyWeather')));
 }
-
-function showPosition(position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    console.log(lat + ' :: ' + lon);
-    const currenUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=5290defddab27e09fb2b75fd44ba25f4&units=metric`;
-    const dailyUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=5290defddab27e09fb2b75fd44ba25f4&units=metric&cnt=7`;
-    getCurrentWeather(currenUrl); // uses the lat and lon from hte showPosition
-    getDailyWeather(dailyUrl);
-}
-
 
 //this runs the api to the get the weather info
 const getCurrentWeather = (url, timeZone) => {
@@ -47,6 +52,7 @@ const getCurrentWeather = (url, timeZone) => {
         if (response.status == 200) {
             console.log('response', response);
             response.json().then(data => {
+                localStorage.setItem('weatherData', JSON.stringify(data))
                 console.log('data', data);
                 getCurrentData(data, timeZone);
 
@@ -59,12 +65,13 @@ const getCurrentWeather = (url, timeZone) => {
     });
 }
 
-const getDailyWeather = ( url, timeZone ) => {
+const getDailyWeather = (url, timeZone) => {
     console.log('dailyUrl:', url)
     fetch(url).then(response => {
         if (response.status == 200) {
             console.log('response', response);
             response.json().then(data => {
+                localStorage.setItem('dailyWeather', JSON.stringify(data))
                 console.log('data', data);
                 getDailyData(data, timeZone);
 
@@ -80,15 +87,19 @@ const getDailyWeather = ( url, timeZone ) => {
 //this gets the api json, parse it and render to the page
 const getCurrentData = (data, location) => {
     let [currentTime] = dateBuilder(data.dt);
+    const check = data.weather[0].description.indexOf('rain');
+    const status = check == -1 ? '<div class="safe">Safe to go out</div>' : '<div class="not-safe">Its Not safe</div>';
+    console.log('check: ', check);
     const output = `
     <section class="location">
         <div class="city">${data.name}, ${data.sys.country}</div>
         <div class="date">${currentTime}</div>
     </section>
     <div class="current">
-        <div class="temp">${Math.round(data.main.temp)}<span>°c<img class="icon" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></span></div>
+        <div class="temp">${Math.round(data.main.temp)}<span>°c<img alt="weather icon" class="icon" src="${api.iconUrl}${data.weather[0].icon}@2x.png"></span></div>
         <div class="weather">${data.weather[0].description}</div>
         <div class="hi-low">${Math.round(data.main.temp_min)}°c/${Math.round(data.main.temp_max)}°c</div>
+        ${status}
     </div>`;
 
     document.querySelector('#main').innerHTML = output;
@@ -96,7 +107,7 @@ const getCurrentData = (data, location) => {
 
 const getDailyData = (data, location) => {
     let output = `
-    <h3>Daily Weather for the Week</h3>
+    <h3 id="dtitle">Daily Weather for the Week</h3>
     <table border="0">
     <thead>
         <th class="align-left">Day</th>
@@ -106,87 +117,84 @@ const getDailyData = (data, location) => {
         <th>Humidity</th>
         <th>Wind</th>
     </thead>
-    <body>`;
+    <tbody>`;
 
-    for ( let i=0; i < data.daily.length-1; i++ ) {
-        let [ currentTime, day ] = dateBuilder(data.daily[i].sunrise);
-     output  += `<tr>
-        <th class="align-left">${day}<br><small>${currentTime.substring(currentTime.indexOf(',')+1,currentTime.lastIndexOf(','), 1)}</small></th>
-        <td class="icon"><small><img class="iconImg" src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png"></small></td>
+    for (let i = 1; i < data.daily.length; i++) {
+        let [currentTime, day] = dateBuilder(data.daily[i].sunrise);
+        output += `<tr>
+        <th class="align-left">${day}<br><small>${currentTime.substring(currentTime.indexOf(',') + 1, currentTime.lastIndexOf(','), 1)}</small></th>
+        <td class="icon"><small><img  alt="weather icon" class="iconImg" src="${api.iconUrl}${data.daily[i].weather[0].icon}.png"></small></td>
         <td><small>${data.daily[i].weather[0].description}</small></td>
         <td>${Math.round(data.daily[i].temp.min)}°c / ${Math.round(data.daily[i].temp.max)}°c</td>
         <td>${data.daily[i].humidity}%</td>
         <td>${data.daily[i].wind_speed}km/hr</td>
         </tr>`;
     }
-      
-    output += `</body>
-              </table>`;
-document.querySelector('#dailyWeather').innerHTML = output;
+
+    output += `</tbody>
+              </table> <br/><br/><br/>`;
+    document.querySelector('#dailyWeather').innerHTML = output;
 }
 
-const searchQuery = ( timeZone ) => {
+const searchQuery = (timeZone) => {
     console.log(timeZone);
     let lon, lat, location;
-    const currenUrl = `https://api.openweathermap.org/data/2.5/weather?q=${timeZone}&APPID=5290defddab27e09fb2b75fd44ba25f4&units=metric`;
-    getCurrentWeather( currenUrl )
+    const currenUrl = weatherByName(timeZone);
+    getCurrentWeather(currenUrl)
     fetch(currenUrl).then(response => {
-      if (response.status == 200) {
-        // console.log('response', response);
-        response.json().then(data => {
-          location = data.name;
-          lon = data.coord.lon;
-          lat = data.coord.lat;
-          let dailyUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=5290defddab27e09fb2b75fd44ba25f4&units=metric`;
-          getDailyWeather( dailyUrl )
-          console.log('login::', lon, 'latit::', lat);
-        }).catch(ex => {
-        //   console.log(ex);
-        });
-      } else {
-        alert('sorry, location not found. Check the spelling and try again!')
-      }
+        if (response.status == 200) {
+            // console.log('response', response);
+            response.json().then(data => {
+                location = data.name;
+                lon = data.coord.lon;
+                lat = data.coord.lat;
+                let [, dailyUrl] = weatherByCoord(lat, lon);
+                getDailyWeather(dailyUrl)
+                console.log('login::', lon, 'latit::', lat);
+            }).catch(ex => {
+                console.log(ex);
+            });
+        } else {
+            alert('sorry, location not found. Check the spelling and try again!')
+        }
     }).catch(err => {
-      console.log(err);
+        console.log(err);
     });
-  }
+}
 
-  const search = document.querySelector('#searchTerm');
-  search.addEventListener('keypress', function(e) {
+const search = document.querySelector('#searchTerm');
+search.addEventListener('keypress', function (e) {
+    
     const searchTerm = search.value;
     if (searchTerm) {
-        if (e.key === 'Enter') searchQuery( searchTerm );
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            searchQuery(searchTerm.trim());
+        }
     }
-  });
+});
 
-  const submit = document.querySelector('#submit');   
-  //  listens for click event from the search box
-  submit.addEventListener('click', function() {
-      const searchTerm = document.querySelector('#searchTerm').value;
-      if (searchTerm) searchQuery( searchTerm );
-      else alert('Please enter a country, city or town');
-  });
+const submit = document.querySelector('#submit');
+//  listens for click event from the search box
+submit.addEventListener('click', function (e) {
+    const searchTerm = document.querySelector('#searchTerm').value;
+    e.preventDefault();
+    if (searchTerm) {    
+        searchQuery(searchTerm.trim());
+    }
+    else alert('Please enter a country, city or town');
+});
 
 const dateBuilder = (currentDate) => {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     let dat = (currentDate) * 1000;
     const dateTime = new Date(dat).toLocaleString();
     const dt = new Date(dat);
     console.log(dt);
     let day = days[dt.getDay()];
-    let currentTime =  `${day}, ${dateTime}`
+    let currentTime = `${day}, ${dateTime}`
 
     return [currentTime, day];
-}
-
-
-
-function myFunction() {
-    setTimeout(showPage, 4000);
-}
-  
-function showPage() {
-    document.getElementById("loader").style.display = "none";
 }
--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,78 @@
-// self.addEventListener('install', event => {
-//     console.log('Service worker installing...');
-//     self.skipWaiting();
-// });
+let filesToCache = resourceToCache();
 
-// self.addEventListener('activate', event => {
-//     console.log('Service worker activating...');
-// });
+const staticCacheName = 'weather-cache-v3';
 
-// // I'm a new service worker
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  
+  self.skipWaiting();
 
-// self.addEventListener('fetch', event => {
-//     console.log('Fetching:', event.request.url);
-// });
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(response => {
+          // Respond with custom 404 page
+          if (response.status === 404) {
+            console.log('404 ', event.request.url, ' not founds');
+            return caches.match('/not-found.html');
+          }
+          return caches.open(staticCacheName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+
+      }).catch(error => {
+        console.log("Hey your app is offline")
+        //Respond with custom offline page
+        return caches.match('/offline.html');
+      })
+  );
+});
 
 
-const filesToCache = [
-    
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+  
+  const cacheAllowlist = [staticCacheName];
+  
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+function resourceToCache() {
+  return [
+    '/',
     'css/main.css',
     'img/pic5.jpg',
     'index.html',
     'js/all.min.js',
     'js/main.js',
-    '404.html',
     'offline.html',
     'android-chrome-192x192.png',
     'android-chrome-512x512.png',
@@ -32,67 +83,7 @@ const filesToCache = [
     'favicon-32x32.png',
     'mstile-150x150.png',
     'safari-pinned-tab.svg',
-    'site.webmanifest'
+    'site.webmanifest',
+    'not-found.html'
   ];
-  
-  const staticCacheName = 'weather-cache-v1';
-  
-  self.addEventListener('install', event => {
-    console.log('Attempting to install service worker and cache static assets');
-    event.waitUntil(
-      caches.open(staticCacheName)
-      .then(cache => {
-        return cache.addAll(filesToCache);
-      })
-    );
-  });
-
-  self.addEventListener('activate', event => {
-    console.log('Activating new service worker...');
-  
-    const cacheAllowlist = [staticCacheName];
-  
-    event.waitUntil(
-      caches.keys().then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            if (cacheAllowlist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    console.log('Fetch event for ', event.request.url);
-    event.respondWith(
-      caches.match(event.request)
-      .then(response => {
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache');
-          return response;
-        }
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request).then(response => {
-            // Respond with custom 404 page
-            if(response.status === 404) {
-                return caches.match('404.html');
-            }
-            return caches.open(staticCacheName).then(cache => {
-              cache.put(event.request.url, response.clone());
-              return response;
-            });
-          });
-  
-      }).catch(error => {
-        console.log("Hey your app is offline")
-        //Respond with custom offline page
-        return caches.match('/offline.html');
-      })
-    );
-  });
-
-
-  
+}
